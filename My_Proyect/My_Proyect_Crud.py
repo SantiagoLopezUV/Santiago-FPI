0ec71c218d8793fddac0d5ddcb898b96f472db6d
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2023-11-22 19:09:22 -0500

    Create Crud

--- My_Proyect/My_Proyect_Crud.py
+++ My_Proyect/My_Proyect_Crud.py
@@ -1,46 +1,29 @@
 #   My Proyect CRUD = Crear, Leer, Actualizar, Eliminar
-clients = ['Luis', 'Kevin', 'Santiago']
+clients = 'Luis, Kevin, Santiago'
 
 def create_client(new_client): # Función Crear
+    global clients 
     if new_client not in clients: # Not in | Pregunta si el primer parametro no esta en el segundo
-        clients.append(new_client)
+        clients += new_client
         get_list_client_names()
     else:
         print('The Client {} already exists'.format(new_client))
 
-
 def read_client(search_client): # Función Leer
-    for search, client in enumerate(clients, 1):
-        print('{}: {}'.format(search, client))
-
-
-def update_client(client_name):  # Función Actualizar
-    if client_name in clients:
-        while(True):
-            upd_client = input('Name to Update: ').capitalize()
-            if upd_client in clients:
-                print('Client Exists')
-            else:
-                index = clients.index(client_name)
-                clients[index] = upd_client
-                get_list_client_names()
-                break
+    global clients
+    if search_client in clients:
+        print('Client Exists')
     else:
         print('Client Not Exists')
 
-
-def delete_client(client_name): # Función Eliminar
+def update_client(client_name,upd_client):  # Función Actualizar
+    global clients
+    clients = clients.replace(client_name,upd_client)
+    
+def delete_client(client_name, del_client): # Función Eliminar
+    global clients
     if client_name in clients:
-        while(True):
-            del_client = input('Name to Delete: ').capitalize()
-            if del_client in clients:
-                clients.remove(del_client)
-                get_list_client_names()
-                break
-            else:
-                print('Clients no Exists')
-                get_list_client_names()
-
+        clients = clients.replace(del_client,"")
 
 def welcome():  # Presentación del Programa
     print('\n\t\t\t\t\tWELCOME TO UNIVERSITY DEL VALLE - COL')
@@ -50,39 +33,47 @@ def welcome():  # Presentación del Programa
     print('[R]ead Client o User: ')
     print('[U]pdate Client o User: ')
     print('[D]elate Client o User: ')
-
-
+    
 def get_name_client():  # Creamos una función que permite preguntar nombre cliente
     return input("Enter your name: ")
 
-
 def get_list_client_names():    # Creamos una función que nos imprima la base de datos
+    global clients
     print(clients)
-
 
 def add_comma():    # Creamos una función para agregar "," y nos modifique la base de datos
     global clients
     clients += ", "
-
 
 if __name__ == '__main__':
     welcome()
     option = input('\nType option desired client: ').upper()
     if option == 'C':
         client_name = get_name_client().capitalize()
+        add_comma()
         create_client(client_name)
-
     elif option == 'R':
         client_name = get_name_client().capitalize()
         read_client(client_name)
-
+        get_list_client_names()
     elif option == 'U':
         client_name = get_name_client().capitalize()
-        update_client(client_name)
-
+        if client_name in clients:
+            upd_client = input('Name to Update: ').capitalize()
+            update_client(client_name,upd_client)
+            get_list_client_names()
+        else:
+            print('User no Exists')
     elif option == 'D':
         client_name = get_name_client().capitalize()
-        delete_client(client_name)
-
+        if client_name in clients:
+            del_client = input('Name to Delete: ').capitalize()
+            if del_client in clients:
+                delete_client(client_name,del_client)
+            else:
+                print('Clients no Exists')
+            get_list_client_names()
+        else:
+            print('User no Exists')
     else:
         print('Command Invalid')
